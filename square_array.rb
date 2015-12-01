809606548813b85a4d8e592a28aef861d62666db
ed-karabinus <ejkarabin@gmail.com>
2015-11-30 23:45:05 -0600

    Finished Square Array Lab; added definition of #square_array using #each to square_array.rb and proposed alternate definition using #collect in a comment.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+# An alternate version of #square_array, using #collect, would be 
+#
+# def alternate_square_array(array)
+#   array.collect { |number| number ** 2 }
+# end
+
 def square_array(array)
-  # your code here
+  new_square_array = []
+  array.each {|element| new_square_array << element ** 2}
+  new_square_array
 end
