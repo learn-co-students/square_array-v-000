291fcf8555ca42c46aea583f168bb7ff9e6a9152
lizdenhup <lizdenhup@gmail.com>
2015-12-14 22:29:30 -0600

    finished Square Array lab

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  new_array =[]
+  array.each { |x| new_array << x ** 2 }
+  new_array
 end
+
+#Bonus - Here is a version of the square_array method which uses the higher
+#level iterator "collect".
+#def square_array_using_collect(array)
+#  array.collect { |x| x ** 2}
+#end
