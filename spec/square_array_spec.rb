b618cfc49f31b936a2fe8bb7acb0abbf8e5e6563
eric-an <erican@me.com>
2015-12-15 09:57:15 -0800

    use each method to square an array's elements

--- spec/square_array_spec.rb
+++ spec/square_array_spec.rb
@@ -1,18 +1,18 @@
 describe '#square_array' do
 
-  # it 'does not call on collect/map or inject' do
-  #   numbers = [1,2,3]
-  #   expect(numbers).to_not receive(:collect)
-  #   expect(numbers).to_not receive(:map)
-  #   expect(numbers).to_not receive(:inject)
-  #   square_array(numbers)
-  # end
+  it 'does not call on collect/map or inject' do
+    numbers = [1,2,3]
+    expect(numbers).to_not receive(:collect)
+    expect(numbers).to_not receive(:map)
+    expect(numbers).to_not receive(:inject)
+    square_array(numbers)
+  end
 
-  # it 'calls on each' do
-  #   numbers = [1,2,3]
-  #   expect(numbers).to receive(:each)
-  #   square_array(numbers)
-  # end
+  it 'calls on each' do
+    numbers = [1,2,3]
+    expect(numbers).to receive(:each)
+    square_array(numbers)
+  end
 
   it 'should square the elements in an array' do
     expect(square_array([1,2,3])).to eq([1,4,9])
--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,20 @@
 def square_array(array)
-  squared_array = array
-  squared_array.collect! do |element|
-    element ** 2
+
+  squared_array = []
+  array.each do |element|
+    squared_array << element ** 2
   end
   squared_array
+  
 end
+
+
+
+
+# def square_array(array)
+
+#   array.collect! do |element|
+#     element ** 2
+#   end
+
+# end
