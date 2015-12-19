08ad35ef25d82246e113914b788d45557285325e
“empireofryan” <ryan@ryanclifton.com>
2015-12-19 04:52:57 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
+  final = []
+  array.each do |a|
+    final << a**2
+  end
+    final
+end
+
+#collect method
+
+def collect_sq_array(array)
+  final = []
+  array.collect do |a|
+    final << a**2
+  end
+    final
 end
