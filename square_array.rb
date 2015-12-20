da652f8e9e8ee8dbce29d08a513b97676bed7356
davdkm <davd.km@gmail.com>
2015-12-19 16:56:23 -0800

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def square_array(array)
   # your code here
+  new_numbers = []
+  array.each{|number| new_numbers << number ** 2}
+  new_numbers
 end
+
+=begin
+def square_array(array)
+  # your code here
+  array.collect{|number| number ** 2}
+end
+=end
