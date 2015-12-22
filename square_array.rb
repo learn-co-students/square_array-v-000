264189e0c33462192bbed02a2e21bc039042fb70
glee38 <glee3388@gmail.com>
2015-12-21 19:32:25 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(numbers)
-  new_array = []
-  numbers.each do |number|
-    new_array << number ** 2
+  new_array = square_array.collect { |number| number ** 2 }
   end
   new_array
 end
