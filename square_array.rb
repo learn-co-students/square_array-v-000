b4c68bc8e2944e4c5f00b6d608c257f14658cc7f
phamducv <phamducv@gmail.com>
2015-12-23 20:42:15 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+        squared = []
+        array.each do |element|
+
+                squared << element ** 2
+
+        end
+        return squared
 end
+
