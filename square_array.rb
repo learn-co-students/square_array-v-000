c693b7e266056291653dee05ef5d46c635553de7
hide83 <hiral.desai83@gmail.com>
2016-02-03 21:08:40 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  i=0
+  array_variable=[]
+
+  array.each do|number|
+    array_variable[i]=number*number
+
+
+i=i+1
+
+
+
+  end
+  return array_variable
 end
