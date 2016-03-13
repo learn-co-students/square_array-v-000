644bc09ddc372a9c674d48c66b2ebdd84e7de1d1
PeteAJ <peter.jacobson@gmail.com>
2016-03-13 13:11:19 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,5 @@
 def square_array(array)
-numbers = [1,2,3]
- new_numbers = [9,10,16,25]
-numbers.each do |new_numbers|
-  puts "the square of #{numbers} is #{new_numbers}"
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
