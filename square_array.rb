b64b786c5ea616c470d9bda7ab3afe50a9052ef7
Shmuwol <shmullywolfson@gmail.com>
2016-03-14 22:14:55 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -3,14 +3,14 @@
   # push element to new array
   # return new array
 
-# def square_array(array)
-# 	squared_nums = []
+def square_array(array)
+	squared_nums = []
 
-#   array.each do |number|
-#     squared_nums << (number * number)
-#   end
-#   squared_nums
-# end
+  array.each do |number|
+    squared_nums << (number * number)
+  end
+  squared_nums
+end
 
 
 # def square_array(array)
@@ -19,10 +19,9 @@
 #     number * number
 #   end
 # end
-
-def square_array(array)
-  array.collect do |number|
-    number * number
-  end
-end
-
+#
+# def square_array(array)
+#   array.collect do |number|
+#     number * number
+#   end
+# end
