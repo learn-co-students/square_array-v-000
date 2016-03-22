1e54c33572c0b9762980affbba5cc8989eaafb79
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-03-21 21:56:44 -0700

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
+def square_array(numbers = [1, 5, 3, 4])
+	counter = 0
+		numbers.each do |number|
+			number = number ** 2
+			numbers[counter] = number
+			counter += 1
+		end
 end
