2432d8cccbc8a38ed4d7d1105555fa6f16de3c9f
lisastar234 <lisa.starzyk@gmail.com>
2016-04-06 11:15:24 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,30 @@
-def square_array(array)
-  # your code here
+
+numbers=[1,2,3]
+
+def square_array(numbers)
+	squared=[]
+ numbers.each {|x| 	 
+ squared << x*x
+}
+ squared
 end
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+
+
+
+
+
+
