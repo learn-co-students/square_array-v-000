ace9a2d2189f72adc4d043e1c1a1b2e26e23d5a8
kaileighrose <kaileighrose@gmail.com>
2016-04-27 22:02:14 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+require 'pry'
+
+def square_array(numbers)
+  new_array = []
+  numbers.each do |x|  
+    puts x **= 2
+    new_array << x
+  end
+  new_array
 end
