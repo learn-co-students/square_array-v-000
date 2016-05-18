72cd144cbd45215e26c79df61df99be75b7f0045
Yechiel Kalmenson <chilikkal@gmail.com>
2016-05-18 17:03:51 -0400

    Square arrays using #each and #collect

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  squared_array = []
+  array.each do |n|
+    n **= 2
+    squared_array << n
+  end
+  squared_array
+end
+
+def square_array_collect(array)
+  # your code here
+  array.collect! do |n|
+    n **= 2
+  end
 end
