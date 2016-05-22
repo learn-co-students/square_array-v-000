289c765ac7d94c929bda871558bfea78950e5739
jack woodward <jackwoodwardschool@gmail.com>
2016-05-21 19:43:27 -0700

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |number|
+      new_numbers = number * number
+      new_array.push(new_numbers)
+  end
+  return new_array
+
 end
