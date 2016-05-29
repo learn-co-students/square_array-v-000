f81d2ce7a94070c17996b059ac25781314d7fd16
Michael Stewart <upp_mike@yahoo.com>
2016-05-28 17:18:28 -0700

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,21 @@
 def square_array(array)
   # your code here
+
+# here's doing it with #collect
+=begin
+    array.collect do |number|
+      number * number
+    end
+=end
+
+  new_array = []
+
+  array.each do |number|
+    new_array << (number * number)
+  end
+
+  new_array
+
 end
+
+square_array([1,2,3])
