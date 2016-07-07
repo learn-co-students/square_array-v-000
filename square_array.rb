6600230173be5ff28cc1ccd6f511aa2cb4907495
Lani1321 <lani.berry13@gmail.com>
2016-07-07 12:54:42 -0400

    iterate over array using .each method

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  num = []
+  array.each {|number| num << number ** 2}
+  num
 end
+
+
+# def square_array(array)
+#  array.map {|num| num ** 2}
+# end
+
+
+
