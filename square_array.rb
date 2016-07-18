597d645e78811d609cb6b2b4c7b04c85a55c7b4d
tylertaylor <vikingtyty@gmail.com>
2016-07-18 11:04:15 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each do |i|
+    squared_array.push(i**2)
+  end
+  squared_array
 end
+
+# Once you have the tests passing, see if you can implement this with a higher
+# level iterator such as .collect
+
+#def square_array(array)
+#  array.collect{|i| i**2}
+#end
