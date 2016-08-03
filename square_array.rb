8ec382c9e25a6555825a3a621dfa37ac76a1e157
Kenlyn Terai <kkterai@gmail.com>
2016-08-03 22:13:35 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
-counter=0
-
 def square_array(array)
-   array.each do |element|
-   array << element**2
-   array.delete_at(counter+1)
+  counter=0
+  array.each do |element|
+  array.unshift(element**2)
+  array.delete_at(counter+1)
   end
   counter+=1
 end
