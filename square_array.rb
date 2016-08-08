e78f0bedaefb7026d55d8ce51af7509b233f535b
Benjamin Cantlupe <bjcantlupe@gmail.com>
2016-08-08 01:58:01 +0000

    square array

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
-def square_array(array)
-  # your code here
-end
+  def square_array(array)
+    new_array = []
+    array.each do |i|
+      num = i * i
+      new_array.push(num)
+    end
+    return new_array
+  end
