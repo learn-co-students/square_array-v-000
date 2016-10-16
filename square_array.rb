a95438b1a6ecae8274729d501be93e35944db6d6
John Espinosa <johnfelix.espinosa@icloud.com>
2016-10-16 06:39:12 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,11 @@ def square_array(array)
   array.each {|num| arrays << num * num}
   arrays
 end
+
+#def square_array(array)
+#  array = []
+#  array.collect do |num|
+#    num * num
+#    array
+#  end
+#end
