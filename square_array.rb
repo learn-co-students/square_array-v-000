c6100427a10e5b5b58ed6384ab4abb503fa81e4a
kelly <github email address>
2016-10-31 06:26:23 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+  newarray=[]
+  counter=0
+  array.each do |array|
+  	
+  	newarray[counter]=array*array
+  	counter+=1
+  end
+  newarray 
 end
+array = [1, 2, 3]
+square_array(array)
