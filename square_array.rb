8fbdbdad78bf1478b2bc68b5a2e86fe9c9d935d4
Callie Brazil <cmabrazil@gmail.com>
2016-11-11 19:29:44 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+numbers = [1, 2, 3, 4, 5]
+
 def square_array(array)
-  # your code here
+  # create new_array
+  new_array = []
+  # square elements of array
+  array.each do |n|
+    n = n ** 2
+    # store them in new_array
+    new_array.push(n)
+  end
+  # return new_array
+  return new_array
 end
