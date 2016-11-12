fd0e0a683a0152a85992d8f25481d7f361286697
Bianca Laurendine <bnlauren@gmail.com>
2016-11-12 22:43:21 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  squared_array=Array.new
+
+  array.each do |number|
+    number=number**2
+    squared_array<<number
+  end
+  squared_array
+end
+
+# altertaive approach, using #collect
+
+def square_array_two(array)
+  array.collect{|number| i**2}
 end
