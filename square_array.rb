3059f789126a59095966ec78c78ae2754ceaf095
Kym Rohman <kymrohman@gmail.com>
2016-11-19 14:37:13 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,26 @@
 def square_array(array)
-  # your code here
+	new_array = []
+	array.each{|num| new_array << num**2}
+	new_array
 end
+
+#this works but doesn't call on each so won't pass tests
+# def square_array(array)
+#     Array.new(array.size) { |i| array[i] ** 2}
+# end
+
+
+# def square_array(array)
+# 	array.each{|num| return num**2}
+# end
+# returns only first number in array
+
+
+# array.each do |num|
+# 	num**2.sort
+# end
+
+# adding a new array
+# array = ["Matz", "Guido"]
+# second_array = Array.new(array) #=> ["Matz", "Guido"]
+# array.equal? second_array       #=> false
