6c72c7470a6be646f9531180e8cea2799f40cb59
mxdavis <md01@me.com>
2017-02-17 01:01:04 +0200

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,13 @@
 def square_array(array)
   new_array = []
-  array.each do |i|
+  array.collect do |i|
     new_array << i * i
   end
   new_array
+end
+
+def collect_square_array(array)
+  array.collect do |i|
+    i * i
+  end
 end
