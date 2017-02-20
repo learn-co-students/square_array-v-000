a66ac86ac87525dd900a276e199140258b55b552
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-02-20 23:04:03 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |number|
+    new_array << number ** 2
+  end
+  new_array
+
+  # advanced code here using higher level iterator
+  # new_array = array.collect{|number| number ** 2}
 end
