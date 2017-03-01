4bb75f68f7c50a16e115dbaff9e74369bca1f38e
thesilentc <thesilentc@me.com>
2017-02-28 20:45:07 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,6 @@
+# example elements for array [1, 2, 3]
 def square_array(array)
-  squared = []
-  array.each { |element| squared << element ** 2 }
-  squared
+  squared = [] # create new empty array
+  array.each { |element| squared << element ** 2 } # 1**1, 2**2, 3**3
+  squared # reurns new array of squared array [1, 4, 9]
 end
