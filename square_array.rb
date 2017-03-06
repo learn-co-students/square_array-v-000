3bf7171e849a974662dc2aa9345768a62c139e7e
Seth Alexander <seth.a.alexander@gmail.com>
2017-03-06 02:01:12 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -3,3 +3,9 @@ def square_array(array)
   array.each {|x| new_array.push(x**2)}
   return new_array
 end
+
+# Using the .collect iterator:
+#
+# def square_array(array)
+#   new_array = array.collect{|x| x**2}
+# end
