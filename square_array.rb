147fa69cc160f888fa38787a1884f764d5642901
Christina Varipapa <christina.varipapa@gmail.com>
2017-03-12 05:01:39 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |number|
+    number **= 2
+    new_array << number
+  end
+  new_array
 end
+
+=begin
+def square_array(array)
+  new_array = array.collect{|number| number **= 2}
+end
+=end
