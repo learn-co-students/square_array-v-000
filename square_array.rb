73af6abe0b15a4a54c4829aa550c60ee93ea0d52
Harleigh Abel <harleighabel@gmail.com>
2017-03-18 08:51:11 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each {|x| new_array << x ** 2}
+  new_array
 end
+
+
+=begin
+Before REFACTORING
+def square_array(array)
+  new_array = []
+  array.each do |x|
+    x = x ** 2
+    new_array << x
+  end
+   return new_array
+end
+=end
