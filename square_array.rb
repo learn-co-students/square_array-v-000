57c8efe6d19786768f24280f46961f65c5bad9a5
Sergio Mendoza <smendoza787@gmail.com>
2017-03-18 03:25:38 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,19 @@
+require 'pry'
+
 def square_array(array)
-  new_array = []
+  new_array = Array.new
   array.each do |num|
     new_array << num**2
   end
+  return new_array
+end
+
+def square_array_with_collect(array)
+  new_array = array.collect { |num|
+    num**2
+  }
 
   return new_array
 end
+
+square_array([1,2,3])
