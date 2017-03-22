49e880d4622e10fc0b3f801c16669fce6f9148f6
Eva Navarror <emanao@me.com>
2017-03-22 16:20:29 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,10 @@
 def square_array(array)
   # your code here
-  new_array = array.each{|num| num**2}
-  array.collect! {|num| num**2}
+  #square_array with each method
+  square_array=[]
+  array.each{|num| square_array<<num**2}
+  square_array
+  #squares with collect method
+  array.collect!{|num|num**2}
 
 end
