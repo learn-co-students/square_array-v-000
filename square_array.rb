eab37de70ab80675b3af360d9882ff3538db17a8
cjbrock <corinna@flatironschool.com>
2017-03-23 11:33:40 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  # longer format answer
+  # array.each do |number|
+  #   number_squared = number*number
+  #   new_array << number_squared
+  # end
+  array.each {|number| new_array << number*number}
+  new_array
 end
