d79a22dd7a60ae803db6dec3b868cae88b7cb265
Julia Thompson <thompson.julia@gmail.com>
2017-04-08 17:44:33 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,19 @@
 def square_array(array)
   new_array = []
   array.each do |element|
-    new_array << element * element
+    new_array << element * element  # or element ** 2
   end
   new_array
 end
 
 def square_array(array)
   array.collect { |element| element * element}
+end
+
+# solution
+
+def square_array(array)
+  squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 end
