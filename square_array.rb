3f451b2292362db2caa8c4e4a23cadc93188aa53
Elena Poletto <epoletto@lstreetc.com>
2017-04-23 23:58:41 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  # original method using each 
+  index = 0
+  array.each do |element|
+  	array[index] = element**2
+  	index += 1
+  end
+  # collect method
+  # array.collect { |x| x**2 }
 end
