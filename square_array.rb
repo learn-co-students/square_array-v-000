ad43625b1a2d8aa85062ebabf2d8a71a4090efda
Sullivan Nolan <nolakat@gmail.com>
2017-05-09 00:38:01 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  def square_array(array)
+    # your code here
+    new_array = Array.new
+  array.each do |num|
+    new_array << num **2
+  end
+  return new_array
+  end
+
 end
