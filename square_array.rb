0dbbbd0242dcb783d61f4155432cac85a8404405
mikka <mikkanthrope@gmail.com>
2017-05-21 10:43:17 -0700

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,5 @@
 def square_array(array)
-  array.each_with_index {|element, i| array[i] = (element*element) }
+  #  array.each_with_index {|element, i| array[i] = (element*element) }
+  squared = []
+  array.each {|element| squared << element**}
 end
