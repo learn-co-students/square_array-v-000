7dfca9118c830ed10341afe19e9e5492852c750e
Nicholas D'Amico <nickalan82@icloud.com>
2017-05-25 14:13:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  squared = []
+    array.each do |item|
+      squared << item * item
+    end
+    squared
 end
+
+# Advanced Challenge
+  # implement with a higher level iterator eq. .collect
+
+# def square_array(array)
+#   array.collect { |x| x * x }
+# end
