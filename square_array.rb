d15c353f6a043da32104b3c55fee23e3855cbf82
NickisKnowledge <lifechangingstuff@gmail.com>
2017-06-12 20:27:20 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,12 @@
+#require 'pry'
+
 def square_array(array)
   # your code here
   array_squared = []
   array.each do |num|
     squared = num * num
     array_squared.push(squared)
+  #binding.pry
   end
   array_squared
 
@@ -11,3 +14,7 @@ end
 
 # test = [1, 2, 3]
 # square_array(test)
+
+def square_array(array)
+  array.collect { |x| x*x } 
+end
