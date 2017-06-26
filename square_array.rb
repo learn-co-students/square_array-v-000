b4b3f22145f105220649af9b3afc58661748a48f
polishpuff <karolina.kumiega@gmail.com>
2017-06-25 22:23:45 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,15 @@
-# def square_array(array)
-#
-#   new_array = []
-#
-#   array.each do |integer|
-#     new_array << integer ** 2
-#   end
-#   return new_array
-# end
-
 def square_array(array)
-  array.collect do |integer|
-    integer ** 2
+
+  new_array = []
+
+  array.each do |integer|
+    new_array << integer ** 2
   end
+  return new_array
 end
+
+# def square_array(array)
+#   array.collect do |integer|
+#     integer ** 2
+#   end
+# end
