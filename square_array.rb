b37c4aaba9f0df35302d7767caae71e9bb12a8cb
Richard Steinbach <steinbach.dick@gmail.com>
2017-06-27 11:53:18 -0700

    use .each and .collect to return squares

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+ sq_array = []
+ array.each {|x| sq_array << (x**2)}
+ return sq_array
+ 
+end
+
+
+def collect_array(array)
+	col_array = array.collect{|x| x**2}
+	return col_array
 end
