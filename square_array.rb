788e2f4221b0c9b9305b74b787a01b177d331b0c
Sydney C <schun08@gmail.com>
2017-07-02 17:43:55 -0400

    Update square_array.rb

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,4 @@
 def square_array(array)
-<<<<<<< HEAD
   squares = []
   array.each do |item|
     squares.push(item**2)
@@ -14,13 +13,5 @@ end
 #end
 
 #def square_array(array)
-=======
-  res = []
-  array.each{|num| res.push(num**2)}
-  return res
-end
-
-#def square_array(array)
->>>>>>> 15fa224fd505d66e895b313540341bffbb2a5f53
   #array.collect{|num| num**2}
 #end
