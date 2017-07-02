3cea3c1a6fde634d51ebee3a3308d672540e1c60
Sydney Chun <schun08@gmail.com>
2017-07-02 21:39:56 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
 def square_array(array)
-  # your code here
+  squares = []
+  array.each do |item|
+    squares.push(item**2)
+  end
+  squares
 end
+
+#def square_array(array)
+  #res = []
+  #array.each{|num| res.push(num**2)}
+  #return res
+#end
+
+#def square_array(array)
+  #array.collect{|num| num**2}
+#end
