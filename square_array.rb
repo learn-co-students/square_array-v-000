d9dbf6c4df560c3890bdaef5631e53d99758ce88
Dominique De Leon <dominique.a.deleon@gmail.com>
2017-07-13 20:48:20 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
+
+
 def square_array(array)
-  # your code here
+  array.each do |element|
+    array[array.index(element)]=element**2
+
+  end
+
 end
+
+# You will build a method, square_array, that squares each element in an array of numbers and
+# returns a new array of these squared numbers. Use an iterator and implement your own logic,
+# don't use any built-in array methods other than .each
