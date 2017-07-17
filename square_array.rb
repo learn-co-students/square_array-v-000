07cef2e55475a633d8fc814169feaeb33826d717
Tyler Buchheim <buchheimt@yahoo.com>
2017-07-17 16:12:09 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  result = []
+  array.each { |x| result << x ** 2}
+  result
 end
+
+def square_collect(array)
+  array.collect { |x| x ** 2}
+end
+
+puts square_collect([1,2,3,4,5])
