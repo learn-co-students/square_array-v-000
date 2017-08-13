6edf636f40ba4f5f5af0ed7fa619000100fb9bde
Jen Poole <contact.vandepool@gmail.com>
2017-08-13 00:34:34 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  new_numbers = []
+
+  array.each do |num|
+    new_numbers << (num ** 2) # add squared values to new array on each iteration
+  end
+
+  return new_numbers # return new array after loop finishes
 end
