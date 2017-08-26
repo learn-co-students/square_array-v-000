3e26922044302510b605c05e2fecd76c46e4b50b
matos89 <matos89@gmail.com>
2017-08-26 16:02:43 -0700

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  newarray =[]
+  array.each do |number|
+    newarray << number ** 2
+  end
+  newarray
+end
+
+def high_level(array)
+  array.map! do |number|
+    number ** 2
+  end
 end
