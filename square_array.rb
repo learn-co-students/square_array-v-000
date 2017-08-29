97c58dfe2719d8571fb6500feaa842eeca372710
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-08-29 04:08:02 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  return_value = []
+  array.each do |array_value|
+    new_value = array_value * array_value
+    return_value << new_value
+  end
+  return return_value
 end
