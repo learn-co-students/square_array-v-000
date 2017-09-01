48e6bf1c0dcd56a94272f4b4754f23fc1ec21ae9
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-09-01 21:57:47 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  original_array = array
+  updated_array = []
+  count = 0
+
+  original_array.each
+  while count <= original_array.length - 1
+    updated_array[count] = (original_array[count] * original_array[count])
+    count += 1
+  end
+
+  return updated_array
 end
