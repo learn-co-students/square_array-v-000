5dde755973abf0eaf4a9a1c5fbb8f1f429bf2f3c
aisayo <aisayo@icloud.com>
2017-09-01 00:42:42 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  #start with an empty array
+  squared_num = []
+  #array.each operates on each element
+  array.each { |x|  squared_num << x ** 2 } #squared_num is the array we want the squares to go into.
+  #new array goes right of shovel and value goes to the left of <<
+    squared_num #dont need to state "return" for ruby. since this is the last line, it will return automatically.
 end
