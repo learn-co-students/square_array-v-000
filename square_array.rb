a3f6a8516c951e2b9b1dcb052eb38818a3a094a3
ndalcin <dalcin.nicole@gmail.com>
2017-09-12 08:09:58 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,7 @@
 def square_array(array)
   finished = []
   array.each do |number|
-    new_number = (number ** 2)
-    finished.push(new_number)
+    finished.push(number ** 2)
   end
   return finished
 end
