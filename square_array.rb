c92f61e67c42240764a87ed6c18a3aa63af492e8
Dasha Nikitina <dashadariallc@gmail.com>
2017-09-13 11:24:50 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |digit|
+    new_array.push(digit**2)
+  end
+  new_array
 end
+
+# def square_array(array)
+#   array.collect {|digit| digit**2}
+# end
