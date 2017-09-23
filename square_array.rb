5139d26198686fdce27ed11a0734b916503b01af
Trisha Garrett <trisha.garrett@gmail.com>
2017-09-23 19:34:25 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  # this method should return 'an' array that has the numbers squared
+  variable = []
+  array.each do|i|
+    # how can we store this value? And what do we use to store multiple values?
+    variable << i**2
+  end
+  # modified array will be returned here
+  variable
 end
