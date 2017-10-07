cb495471825391a35262036640b1ff44fe5a6ad1
Anastasia  Barkett <anastasia.barkett@gmail.com>
2017-10-07 23:37:35 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,16 @@
 def square_array(array)
   new_array = []
-  array.each do |number|
-  squared = number**2
-  new_array << squared
-  end
-  return new_array
+  array.each {|number| new_array << number**2}
+  new_array
 end
+
+
+
+#def square_array(array)
+#  new_array = []
+#  array.each do |number|
+#  squared = number**2
+#  new_array << squared
+#  end
+#  return new_array
+#end
