6a711ae0b64683cef1b7ffc8b5b676fe9b13c021
Richard Dotson <rich@cityofla.net>
2017-10-10 00:44:58 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  squared = []
+array.each do |numbers|
+  squared << numbers **2
+#squared << (numbers * numbers) working method
+
 end
+squared
+end
+####implement using .collect iterator
+####def square_array(array)
+####  squared = []
+####array.collect {|numbers| numbers **2}
+####end
---
+++ work.rb
@@ -0,0 +1,23 @@
+###########################
+def square_array(array)
+  # your code here
+  squared = []
+array.each do |numbers|
+  squared << numbers **2
+#squared << (numbers * numbers)
+end
+squared
+end
+
+###########################
+def square_array(array)
+  # your code here
+array.each do |square_array|
+  numbers ** (2.0/1.0)
+end
+def square_array(array)
+  # your code here
+array.each do |numbers|
+puts numbers * numbers
+end
+end
