f4cd20c0e04d047f1d05f978c40cb141beaae57a
Parker Catalano <catalano.work@gmail.com>
2017-10-17 19:14:44 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 def square_array(array)
-  # your code here
+  new_array = Array.new
+  array.each do |x|
+    new_array << x ** 2
+  end
+  return new_array
 end
+
+# ruby square_array.rb
