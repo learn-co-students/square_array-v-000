6c7a713c56c8f3ac33dde1deda6e5d1d4f7a0684
Elle Faraday <faraday.elle@gmail.com>
2017-10-28 02:45:44 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  squared = []
+  array.each {|x| new_array << x**2}
 end
+
+def square_array_with_collect(array)
+  array.collect {|x| x**2}
+end
+
+array = [1, 2, 3]
+puts square_array_with_collect(array).inspect
