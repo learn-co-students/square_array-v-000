e27b31aaf88521d0463ee432f50b6eebc0258578
Richard Schaubeck <richard.schaubeck@gmail.com>
2017-11-08 20:43:05 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,11 @@
 def square_array(array)
   sq_array = []
   array.each {|num| sq_array.push(num ** 2)}
-  return sq_array    
+  return sq_array
+end
+
+foo = [3, 2, 6]
+
+def sq_array_with_collect(array)
+  array.collect {|num| num ** 2}
 end
