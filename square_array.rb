6886afdcdc5bbb6967eb061d9238dc8f402ac565
Lekan Nicholson <lekan.nicholson@gmail.com>
2017-11-11 21:02:36 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  square_array = [] #I create a new array to nest the square elements of the original array
+  array.each do |number| #I use the each iterator to go through each element of the array and square it
+    square = number * number #I nest the squared number in a variable that I push into my new array (square_array)
+    square_array.push(square)
+  end
+  return square_array #I return the new array
 end
