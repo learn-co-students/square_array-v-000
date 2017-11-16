2f45681db999cc4c5116e25b8faecea77146aeac
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-16 03:39:10 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
-def square_array(array)
-  # your code here
+
+
+def square_array(numbers)
+  counter = 0
+  numbers.each do |number|
+    number = number.to_i
+    numbers[counter] = number * number
+    counter +=1
+  end
+end
+
+
+def square_array2(numbers)
+  numbers.collect do |number|
+    number = number.to_i
+    number = number * number
+  end
 end
