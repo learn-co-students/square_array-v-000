04992e619a4bcb5328d74cb5e93f94831debc626
Eric Johnson <0216johnsoneric@gmail.com>
2017-11-18 23:01:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+   newArray = []
+   array.each {|x| newArray << x**2 }
+   return newArray
+end
+
+def square_array(array)
+  array.collect {|x| x**2}
+  return array
 end
