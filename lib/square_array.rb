cc7093f9ceb6a07bc3a6ff56df1954c7f55ba290
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2017-12-05 03:16:32 +0000

    Done.

---
+++ lib/square_array.rb
@@ -0,0 +1,7 @@
+def square_array
+  square_array = [1,2,3]
+  square_array.each do |numbers|
+
+  end
+
+end
--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
   # your code here
+  square_array = [1,2,3]
+  square_array.each do |array|
+
+  end
+
 end
