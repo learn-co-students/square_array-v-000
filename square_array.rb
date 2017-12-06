54e0fa18363cea8a9bbc097461b4e4f71fffe605
Eddie Melo <heriberto@melo.nyc>
2017-12-06 00:41:06 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,9 @@
 def square_array(array)
   # your code here
-  array2 = []
-  array.each do |number|
-    array2 << number ** 2
-  end
-  array2
+  squared = []
+  #array.each do |number|
+    #squared << number ** 2
+  #end
+  array.each { |number| squared << number ** 2 }
+  squared
 end
