2d877e38e54fff7e52d831aa889d80888836664c
Lena Sawyer <lenasawyer93@gmail.com>
2017-12-08 20:40:44 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each {|num| new_array<<num**2}
+  new_array
 end
+
+
+#def square_array(array)
+#  array.each_index do |num|
+#    puts "#{num**2}"
+#  end
+#end
