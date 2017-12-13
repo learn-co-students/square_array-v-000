f893705be4e6b646e162f640405fbc106977c69f
kostanzhoglo <jeffreywithers@gmail.com>
2017-12-13 16:47:49 -0500

    Solve lab

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
 def square_array(array)
-  # your code here
+  # your code here]
+  new_array = []
+  array.each do |n|
+    new_array << (n ** 2)
+  end
+  new_array
 end
+
+
+# def square_array(array)
+#   # your code here]
+#   new_array
+#   array.collect do |n|
+#     n ** 2
+#   end
+#   new_array
+# end
