e94ad8a2fd58cad951e2edecd22f8d775ea62f8c
Sam Kalum <stkalum@gmail.com>
2018-01-04 16:17:51 -0500

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
 def square_array(array)
-  # your code here
+  squared = []
+
+  array.each {|num| squared << num**2}
+
+  squared
+end
+
+def square_array_with_collect(array)
+  array.collect {|num| num**2}
+end
+
+def square_array_with_map(array)
+  array.map {|num| num**2}
+end
+
+def square_array_with_inject(array)
+  array.inject([]) {|memo, num| memo << num**2}
 end
