3445782f7909541ee152b79d209777c9adf6b73e
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-04 13:39:01 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+    square_array = []
+    array.each { |num| square_array <<  num * num }
+    return square_array
 end
+
+new_array = []
+array = [2, 3, 4]
+new_array = square_array(array)
+puts "array #{new_array}"
