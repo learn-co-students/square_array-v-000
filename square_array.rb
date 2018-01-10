25d9a3004309de3995149689b78060af4229b638
Allyson Wesman <allysonwesman@gmail.com>
2018-01-09 22:37:57 -0800

    implemented square array

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  squared = []
+  count = 0
+
+  array.each do |number|
+    squared[count] = number ** 2
+    count += 1
+  end
+
+  return squared
 end
