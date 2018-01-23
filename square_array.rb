1b06419f92589aa4053cd377d43c897b3ee1ee9a
Trac Nguyen <nguyendtrac@gmail.com>
2018-01-23 23:33:41 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  squared_array = []
+  counter = 0
+  array.each do |x|
+    x**
+    array[x] = squared_array[0]
+    counter += 1
+  end
+  squared_array
 end
