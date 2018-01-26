e9c73cd9b640006926511fcd0e6e14002cf75b4b
Rollin Metzger <rollin.metzger@gmail.com>
2018-01-26 20:51:09 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
   # your code here
+  square_array = []
+  array.each {|int| square_array << int ** 2}
+  return square_array
+end
+
+def square_array_with_collect(array)
+  square_array = array.collect {|int| int ** 2}
 end
