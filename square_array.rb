f0a347f1c33f569962b782b32008505b07ab13db
Brandon Chin <brandonchin@me.com>
2018-02-09 00:26:25 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,8 @@
 require 'pry'
 def square_array(numbers)
-  new_numbers = []
+  new_array = []
   numbers.each do |square|
-
-    new_numbers.push(square *= square)
-
+    new_array.push(square * square)
   end
-  new_numbers
+  new_array
 end
