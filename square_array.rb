9bedcbf010cf24973d11c26fb296eda59a7f0113
David Markel <davidgmarkel@gmail.com>
2018-03-09 04:52:42 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,5 @@
 def square_array(array)
   squared_array = []
-  array.each do |number|
-  squared_array.push(number * number)
-  end
+  array.each {|number| squared_array << number * number}
   squared_array
 end
