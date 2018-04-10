d6e36fbcc0273bba77247c6d09de9f9f10305c22
Anuradha Khambete <anukhambete@gmail.com>
2018-04-10 22:00:52 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+ 
+  sqr_array = []
+  array.each do |i|
+    sqr = i**2
+    sqr_array.push(sqr)
+  end
+  sqr_array
+    #array.collect {|i| i**2}
 end
