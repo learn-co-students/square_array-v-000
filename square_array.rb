860d1023b267280aea647b2845674d9a0f7fa784
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-26 21:56:01 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -6,3 +6,12 @@ def square_array(array)
   end
   return new_array  # your code here
 end
+
+
+def square_array(array)
+  new_array = []
+  array.each do |element|
+    new_array << element**2
+  end
+  new_array
+end
