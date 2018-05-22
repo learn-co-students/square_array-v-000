ac79cd87bef27efe03be2c27397097bcad59eeaa
Bryan McAfee <brymcafee@gmail.com>
2018-05-22 14:37:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,5 @@
-def square_array(array)
-  squared = []
-  array.each { |numbers| squared << numbers ** 2 }
-  squared
+def square_array(numbers)
+  arr = []
+numbers.each { |numbers| arr.push numbers ** 2 }
+arr
 end
