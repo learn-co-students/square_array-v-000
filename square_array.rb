d07ebe857181c997b0d51895be1cc17d1cb98810
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-05-28 18:21:23 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+    squared_numbers = []
+    numbers.each do | element |
+      squared_numbers << element ** 2
+  end
+  squared_numbers
 end
