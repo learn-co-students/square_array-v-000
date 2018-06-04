606ee2d9bbb4764d6a724b8c60a1bddd919ce27d
Anusha Konatala <anu.konatala@gmail.com>
2018-06-04 01:43:25 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
   # your code here
+  squareArray =[]
+  #array.each {|x| squareArray.push(x*x) }
+  squareArray = array.collect { |x| x * x }  
+  squareArray
 end
+
+puts "#{square_array([1,2,3,4])}"
