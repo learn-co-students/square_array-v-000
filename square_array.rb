6aa2b95469ff5064fe7a6d89dd8207b3d6b2af9e
sharonrieger <sharonarieger@gmail.com>
2018-06-10 10:00:38 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@
 def square_array(numbers)
     squared_numbers = []
     numbers.each do|num|
-      #add squared numbers to the array with push/shovel
+      #add the actual squared numbers to the squared_numbers array with push/shovel
       squared_numbers << num ** 2
     end
     #return the array without []. We don't have to use the [], that's only necessary with arguments/parameters in a method.
