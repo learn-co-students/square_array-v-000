3e0dcd7563126a7c77835d3abab189430b2c47ea
Edsel Caprice <caprice@mail.usf.edu>
2018-06-17 17:38:01 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |square|
+        new_array << square ** 2
+  end
+  return new_array
+end
+
+
+def square_array_collect(array)
+  # your code here
+  array.collect do |square|
+    square ** 2
+  end
 end
