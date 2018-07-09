88487866236b2951a07b2d6d4fc42e91a0a0ce66
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-09 17:16:24 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
-def square_array(array)
-   array.each do |squared|
-   puts squared**2
-   end
+def square_array(numbers)
+  new_array = []
+  numbers.each do |squared|
+  new_array << squared**2 
+  puts new_array
+  end
 end
-   
