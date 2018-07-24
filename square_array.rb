5b6b35f55a5b5cb0023cb9d83f87b5c34f4b4395
Benjamin Jones <bljones36@gmail.com>
2018-07-24 01:41:50 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |x|
+    square = x*x
+    new_array << square
+  end
+  new_array
 end
+
+#def square_array(array)
+#array.collect {|x| x*x }
+#end
