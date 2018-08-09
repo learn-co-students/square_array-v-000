907fd7bf3cfcb2c0809939e5c72556b3c804e42e
Steven Crouse <sdcrouse91@gmail.com>
2018-08-09 00:41:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
+  squares = []
+  array.each {|number| squares.push(number ** 2)}
+  squares
+  
+  # array.collect {|number| number ** 2}
+  # array.map {|number| number ** 2}
+  
+  # array.inject([]) {|squares, number| squares.push(number ** 2)}
+  # Interesting. The code above passes the "calls on each" test.
+  # Maybe #inject calls on #each as part of its implementation!
+  
+  # Note: if I want to do this with strings, here's how:
+  # array.collect { |item| item * 2 } # Remember: ** is undefined for strings!
+  # If item == "word", then item * 2 returns "wordword".
 end
