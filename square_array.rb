c8d3d57a649e265909b2400980698da822775c69
Susan Meyers <smmeyers24@gmail.com>
2018-08-12 18:35:37 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  #define x
+  #use a method to square number for eacn num in array
+  arr = []
+  numbers.each do |num|
+  arr << num ** 2
+  end
+  arr
 end
+
+#Collect can be used also, it returns new array automatically
+#do not need new array when using collect
+#numbers.collect. do |num|
+  #num ** 2
+  #end
+#end
