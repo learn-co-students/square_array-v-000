054cd3697fa65a43b05ffae2d63f448ee04092a3
Laura Darlak <laura@polyporedesign.com>
2018-08-17 13:45:40 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,17 +1,27 @@
-# square array using each iterator
 
+# square array using each iterator
+#
+# def square_array(array)
+#   # create new array
+#   new_array = []
+#   # iterate over each number in the array
+#   array.each do |number|
+#     # square each number in the array and save value to a new variable
+#     new_number = number**2
+#     # shovel squared number variable into the new, empty array
+#     new_array << new_number
+#   end
+#   # return new array
+#   new_array
+# end
+
+# refactored
 def square_array(array)
-  # create new array
-  new_array = []
-  # iterate over each number in the array
-  array.each do |number|
-    # square each number in the array and save value to a new variable
-    new_number = number**2
-    # shovel squared number variable into the new, empty array
-    new_array << new_number
+  squared = []
+  array.each do |element|
+    squared << element ** 2
   end
-  # return new array
-  new_array
+  squared
 end
 
 # ADVANDED: square array using collect iterator
