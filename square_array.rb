3f99a906a4ab0f921251c379ddab755ea85fc391
Dan Hintz <dhintz@live.com>
2018-08-26 20:08:59 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  squared_array = []
+  array.each do |item|
+    squared_array << (item**2)
+  end
+  return squared_array
+end
+
+def collect_square_array(array)
+  array.collect {|item| item**2}
 end
