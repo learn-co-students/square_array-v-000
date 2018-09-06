feb18f2d7a17e4ca29eda3b273b4a4b5ca370ec5
Tim Knoll <timothy.knoll@gmail.com>
2018-09-06 19:51:56 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,17 @@
+# def square_array(array)
+#   new_array = []
+#
+#   array.each {|arr|
+#     new_array.push (array * array)}
+#
+#   return new_array
+# end
+
 def square_array(array)
-  # your code here
+  squared_array = []
+  array.each do |num|
+    square = num ** 2
+    squared_array << square
+  end
+  squared_array
 end
