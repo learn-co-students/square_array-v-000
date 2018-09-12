6e98c000f056c5e73a67f930cb01a44465d99102
Sherry Martin <sm674@cornell.edu>
2018-09-12 22:07:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
-def square_array(array)
-  # your code here
+def square_array(array)  #.map would be more appropriate; however, this lesson called for using .each method
+  array.each_with_object([]) { |i,arr| arr << i ** 2 }
+end
+
+def square_array(array) #alternate way of doing things
+ array.collect {|x| x**2}
 end
