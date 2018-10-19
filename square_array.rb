4cdb5e488a63062b7d4cbcd3b83e10cccb00fc0c
Lois Lee <lois.lee@me.com>
2018-10-19 23:00:31 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,8 @@
 def square_array(array)
   new_array = []
-  array.each do |array|
-    new_array.push(array**2)
+  array.each do |num|
+     new_num = num ** 2
+     new_array << new_num
   end
   new_array
 end
-
-#def square_array(array)
-  # array.collect {|array| array**2}
-#end
