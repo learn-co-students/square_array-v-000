f498e1a802eb3449475f9f310a67dcb095a645de
Kimberly Aviles <01010110.v@gmail.com>
2018-11-02 18:11:49 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  array.each { |x| x * x }
+  array.each { |x| x ** 2 }
 end
+
+
+
+count = array.count
+count_to_index = count - 1
+array[count_to_index] = array[count_to_index] * array[count_to_index]
+count -= 1
