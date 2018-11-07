c1e7522b5beaa922dd23db5649a9fb2b591e6d5e
Kimberly Aviles <01010110.v@gmail.com>
2018-11-07 00:01:13 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,15 @@
+
+
 def square_array(array)
-  array.each { |x| x ** 2 }
+# create a second array variable with no value
+  array2 = []
+# iterate over each element in the given & original array
+    array.each do |x|
+# in each iteration, multiply each element by itself and then assign that value back to the original iterated variable
+      x = x * x
+# shovel (I think it's considered a destructive operation???) each iterated value into the end of the array
+      array2 << x
+    end
+# returns the new array which contains all the values of the array squaring itself
+  return array2
 end
-
-
-
-count = array.count
-count_to_index = count - 1
-array[count_to_index] = array[count_to_index] * array[count_to_index]
-count -= 1
