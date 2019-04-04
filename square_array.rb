a72a9bee8a7a0ca0fad7a24d4ffc354339b2d21c
ALVIN LU <lualvin1234@gmail.com>
2019-04-04 20:13:30 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
-  new_array=[]
-  array.each do |number|
-    new_array.push(number*number)
-    end
-    return new_array# your code here
+  new_array = []
+  array.each do |num|
+    new_array << num*num
+  end
+  new_array
+
 end
